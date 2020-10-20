59884742bee6dbd480710ac2751ed28206eb8ed8
kunal266 <kunalm039@gmail.com>
2020-10-20 16:10:32 +0530

    Updates

---
+++ precache-manifest.6157f50c99421dc9431e837e13d16a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ad3b78e8590c309b1dd3b621ac437b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c5d8ea492f644a2f06a9",
+    "url": "/Portfolio/static/css/main.84e63667.chunk.css"
+  },
+  {
+    "revision": "ab010fee5ffd2cf06bf3",
+    "url": "/Portfolio/static/js/2.f68ab4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.f68ab4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d8ea492f644a2f06a9",
+    "url": "/Portfolio/static/js/main.12ae339d.chunk.js"
+  },
+  {
+    "revision": "efed155d89bc85b81984",
+    "url": "/Portfolio/static/js/runtime-main.88fabfc4.js"
+  }
+]);
